53ea162a63113b580310e6662bded4c81b5bba67
Ifeoma Okafor <ifytrez96@gmail.com>
2024-01-30 10:16:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import 'react-notifications-component/dist/theme.css';
+import { Container, Form, Button, Card } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
+  const [medications, setMedications] = useState([]);
+  const [medicationName, setMedicationName] = useState('');
+  const [dosage, setDosage] = useState('');
+  const [schedule, setSchedule] = useState('');
+
+  useEffect (() => {
+    loadFromLocalStorage()
+  }, []);
+
+  const addMedication = () => {
+    if (medicationName && dosage && schedule){
+      const newMedication = {
+        name : medicationName,
+        dosage : dosage,
+        schedule : schedule
+      }
+    
+
+      setMedications([...medications, newMedication])
+      setReminders(newMedications);
+      saveToLocalStorage();
+      clearForm();
+    } else {
+      alert('Please fill in all fields')
+    }
+  };
+
+  const setReminders = (medication) => {
+    NotificationManager.info(`Time to take ${medication.name} - ${medication.dosage}`, 'Medication Reminder')
+  }
+  const saveToLocalStorage = () => {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
